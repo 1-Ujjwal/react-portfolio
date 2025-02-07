3e7243ddbc9089c8b79d30c63a673b35283f17dd
1-Ujjwal <kingayushman7@gmail.com>
2025-02-07 13:16:52 +0530

    Created About Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/sections/Navbar";
 import MobileMenu from "./components/sections/MobileMenu";
 import Home from "./components/sections/Home";
+import About from "./components/sections/About";
 import { useState } from "react";
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
       </div>
     </>
   );
